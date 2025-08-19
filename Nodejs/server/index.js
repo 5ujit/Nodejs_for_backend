d9fb848b1a84ade2717747bd6614ca7d35bcadf7
5ujit <sksujit021@gmail.com>
2025-08-19 23:29:19 +0530

    node server

--- Nodejs/server/index.js
+++ Nodejs/server/index.js
@@ -1,8 +1,24 @@
-const http = require("http")
+const http = require("http");
 
-const myServer=http.createServer((req,res)=>{
-    console.log("New Req Rec.");
-    res.end("Hello form Server")
+const myServer = http.createServer((req,res)=> {
+    console.log("new request rec   ");
+    res.end("Hello form server")
+
+
+});
+
+
+myServer.listen(8000,()=>console.log("server started"));
+
+
+
+
+
+// const http = require("http")
+
+// const myServer=http.createServer((req,res)=>{
+//     console.log("New Req Rec.");
+//     res.end("Hello form Server")
     
-})
-myServer.listen(8000,()=>console.log("Server Started"))
+// })
+// myServer.listen(8000,()=>console.log("Server Started"))
