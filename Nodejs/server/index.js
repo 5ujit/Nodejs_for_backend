5fb7b96293ae1f1624fdbd569d123608318ad09c
5ujit <sksujit021@gmail.com>
2025-08-20 22:41:47 +0530

    http server

--- Nodejs/server/index.js
+++ Nodejs/server/index.js
@@ -1,14 +1,27 @@
 const http = require("http");
+const fs = require ("fs");
+const { error } = require("console");
 
 const myServer = http.createServer((req,res)=> {
-    console.log("new request rec   ");
-    res.end("Hello form server")
-
-
+    const log = `${Date.now()}:${req.url} New Req Received\n `;
+    fs.appendFile("log.txt", log,(error, data)=>{
+        switch(req.url){
+    case "/": 
+        res.end("HomePage")
+        break;
+    case "/about":
+         res.end("how are u ");
+        break;
+    default:
+        res.end("404 Not Found")
+        }
+       
+    });
 });
 
 
-myServer.listen(8000,()=>console.log("server started"));
+myServer.listen(8000,
+    ()=>console.log("server started"));
 
 
 
