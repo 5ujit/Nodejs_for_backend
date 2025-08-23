e7c2d319c91e98ac50eafd2b3da6517ac53717d1
5ujit <sksujit021@gmail.com>
2025-08-23 22:25:46 +0530

    url

--- Nodejs/server/index.js
+++ Nodejs/server/index.js
@@ -1,16 +1,22 @@
 const http = require("http");
 const fs = require ("fs");
+const url = require("url")
 const { error } = require("console");
 
 const myServer = http.createServer((req,res)=> {
+    if(req.url==="/favicon.ico") return res.end();
     const log = `${Date.now()}:${req.url} New Req Received\n `;
+    const myUrl = url.parse(req.url ,true);
+    console.log(myUrl);
+    
     fs.appendFile("log.txt", log,(error, data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
     case "/": 
         res.end("HomePage")
         break;
     case "/about":
-         res.end("how are u ");
+        const username= myUrl.query.myname;
+         res.end(`Hi, ${username}`);
         break;
     default:
         res.end("404 Not Found")
