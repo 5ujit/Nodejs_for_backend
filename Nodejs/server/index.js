3e5ba839ad60d303b655471718617852f98091cb
5ujit <sksujit021@gmail.com>
2025-08-28 23:49:34 +0530

    express started

--- Nodejs/server/index.js
+++ Nodejs/server/index.js
@@ -1,23 +1,48 @@
-const http = require("http");
+// const  express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+const express = require("express")
+ 
+
+
+
+/*const http = require("http");
 const fs = require ("fs");
 const url = require("url")
 const { error } = require("console");
 
 const myServer = http.createServer((req,res)=> {
     if(req.url==="/favicon.ico") return res.end();
-    const log = `${Date.now()}:${req.url} New Req Received\n `;
+    const log = `${Date.now()}:  ${req.method}  ${req.url} New Req Received\n `;
     const myUrl = url.parse(req.url ,true);
-    console.log(myUrl);
+    // console.log(myUrl);
     
     fs.appendFile("log.txt", log,(error, data)=>{
         switch(myUrl.pathname){
     case "/": 
+    if(req.method=== 'GET') res.end('HomePage')
         res.end("HomePage")
         break;
     case "/about":
         const username= myUrl.query.myname;
          res.end(`Hi, ${username}`);
         break;
+    case "/search":
+        const search=myUrl.query.search_query;
+        res.end("Here are your results for "+ search);
+    case '/signup':
+        if(req.method === 'GET') res.end("This is sighnup form ")
+            else if ( req.method === 'POST '){
+        // DB Query
+        res.end("success ")
+        }
+
     default:
         res.end("404 Not Found")
         }
@@ -41,3 +66,6 @@ myServer.listen(8000,
     
 // })
 // myServer.listen(8000,()=>console.log("Server Started"))
+
+
+*/
