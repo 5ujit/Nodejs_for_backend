cfa3b2a43fdce7d41e169f6e3f8b8b6529a09ca4
5ujit <sksujit021@gmail.com>
2025-09-11 23:08:41 +0530

    Create server.js

---
+++ Nodejs/express/server.js
@@ -0,0 +1,20 @@
+// index.js
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let users = [{ id: 1, name: "Sujit" }];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", (req, res) => {
+  const user = { id: users.length + 1, name: req.body.name };
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
