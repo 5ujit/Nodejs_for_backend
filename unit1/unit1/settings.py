5e49ba5727c68e6396a519a8bba2683ce145bd9d
5ujit <sksujit021@gmail.com>
2025-09-16 23:48:40 +0530

    Update settings.py

--- unit1/unit1/settings.py
+++ unit1/unit1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',  # <- yaha add karo
+    'myapp',
 ]
 
 MIDDLEWARE = [
