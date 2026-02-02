4d02ebafff29f075f3f01d0d5ce8518d5499a854
5ujit <sksujit021@gmail.com>
2026-02-02 21:43:35 +0530

    rest api

--- Nodejs/vid_13REST API/index.js
+++ Nodejs/vid_13REST API/index.js
@@ -6,16 +6,25 @@ const PORT= 3000;
 
 //ROUTES
 
-app.get('/users', (req,res)=>{
-    return res.json()
+app.get("/user",(req,res)=>{
+    const html =`
+
+    <ul>
+    ${users.map((user)=> `<li> ${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html)
+});
+
+app.get('/api/users', (req,res)=>{
+    return res.json(users)
 })
 
+app.get('/api/users/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const user=users.find((user)=> user.id===id);
+    return res.json(user)
 
-
-
-
-
-
-
+})
 
 app.listen(PORT, ()=>console.log(`Server started at port ${PORT}`) )
